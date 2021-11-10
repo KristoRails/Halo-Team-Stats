4e25176c3fa70e31e9a9e8fcdf5ef3bd33d1a667
Chris // Kristo <vasschris69@gmail.com>
2021-11-10 16:42:45 +0200

    Version 0.1
    Added the following:
    Header Navigation, Player Picture Container.
    
    All strings must have their font-changed (TBD)

---
+++ php_tags/header.php
@@ -0,0 +1,20 @@
+<?php 
+
+function callHeader()
+{
+    return '<div class="container-fluid">
+    <div class="container header-container d-flex justify-content-between">
+        <h1>Demonica Team Stats</h1>
+        <ul class="nav">
+            <li class="nav-item">
+                <a href="#">Player Stats</a>
+            </li>
+            <li class="nav-item">
+                <a href="#">Team Stats</a>
+            </li>
+        </ul>
+    </div>
+</div>';
+}
+
+?>
